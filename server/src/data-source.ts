d4dd95cc0c520be8f83365a689e4d6375f1bcea5
ex1 <skowko6750@naver.com>
2022-10-23 17:23:49 +0900

    DB, 애플리케이션 연결 (TypeORM 설정)

---
+++ server/src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entities/User"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "password",
+    database: "postgres",
+    synchronize: true,
+    logging: false,
+    entities: ["srv/entities/**/*.ts"],
+    migrations: [],
+    subscribers: [],
+})
