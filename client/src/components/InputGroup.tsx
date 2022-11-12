aee659aabaf10645d5c497631c0aebb7104c57d5
ex1 <skowko6750@naver.com>
2022-11-12 17:15:14 +0900

    inputGroup Component 생성

---
+++ client/src/components/InputGroup.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import cls from "classnames";
+
+interface inputGroupProps {
+    className?: string;
+    type?: string;
+    placeholder: string;
+    value: string;
+    error: string | undefined;
+    setValue: (str: string) => void;
+}
+
+const inputGroup: React.FC<inputGroupProps> = ({
+    className = "nb2",
+    type = "text",
+    placeholder = "",
+    value,
+    error,
+    setValue
+}) => {
+  return (
+    <div className={className}>
+        <input
+            type={type}
+            style={{minWidth:300}}
+            className={cls (`w-full p-3 transition duration-200 border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+                {"border-red-500": error}
+            )}
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+        <small className='font-medium text-red-500'>{error}</small>
+    </div>
+  )
+}
+
+export default inputGroup
