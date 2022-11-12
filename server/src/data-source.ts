fe9b0b8e905232a81e81c279b995cac9c1048462
ex1 <skowko6750@naver.com>
2022-11-12 17:13:10 +0900

    import 에러 수정

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entities/User"
+import User  from "./entities/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
