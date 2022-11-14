11968f8661f14f7536e761a805188eb638d46733
ex1 <skowko6750@naver.com>
2022-11-14 14:53:19 +0900

    로그아웃 쿠키 만료 구현

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -96,9 +96,23 @@ const login = async(req: Request, res: Response) => {
     }
 }
 
+const logout = async(_eq: Request, res: Response) => {
+    res.set(
+        "Set-Cookie",
+        cookie.serialize("token", "", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            expires: new Date(0),
+            path: "/"
+        })        
+    )
+    res.status(200).json({ success:true })
+}
 const router = Router();
 router.get("/me", userMiddleware, authMiddleware, me);
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", userMiddleware, authMiddleware, logout)
 
 export default router;
