221b9c8c1e174a3a851cf778a17a5d255d517361
ex1 <skowko6750@naver.com>
2022-11-23 16:29:09 +0900

    .

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     app:[{
-        name:"reddit-client"
+        name:"reddit-client",
         script: "npm run start:prod"
     }]
 }
--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     app:[{
-        name:"reddit-server"
+        name:"reddit-server",
         script: "npm run start:prod"
     }]
 }
