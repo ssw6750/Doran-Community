f65fbc3bf16b20be931e6731bba0e6da992cc08f
ex1 <skowko6750@naver.com>
2022-11-23 15:07:07 +0900

    유저페이지 구현

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth'
 import subRoutes from './routes/subs'
 import postRoutes from './routes/posts'
 import voteRoutes from './routes/votes'
+import userRoutes from './routes/users'
 import cors from 'cors';
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
@@ -29,6 +30,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/subs", subRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/votes", voteRoutes)
+app.use("/api/users", userRoutes)
 
 let port = 4000;
 app.listen(port, async () => {
