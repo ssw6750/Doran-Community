ac718daf1e143ec3aa7c9f8a84004181c0c54154
ex1 <skowko6750@naver.com>
2022-11-25 17:41:16 +0900

    커뮤니티 전체리스트 생성

---
+++ client/src/pages/subs/list.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotImplemented from '../test/not_implemented';
+
+const List = () => {
+  return (
+    <div>
+        <NotImplemented></NotImplemented>
+    </div>
+  )
+}
+
+export default List;
