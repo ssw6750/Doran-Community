51dd50b98d2e4869f9b6bbcb266e7a424e8ffa82
ex1 <skowko6750@naver.com>
2022-11-25 17:40:30 +0900

    메인페이지 수정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -76,7 +76,7 @@ const Home: NextPage = () => {
 
       {/* 사이드바 */}
       <div className='hidden w-4/12 ml-3 md:block'>
-        <div className='bg-white border rounded'>
+        <div className='bg-white border rounded mb-6'>
           <div className='p-4 border-b rounded-t'       
             style={{
               backgroundImage: `linear-gradient(rgba(135, 80, 156, 0.9), rgba(135, 80, 156, 0.9)), url(/tree-2249363_1920.jpg)`,
@@ -117,16 +117,51 @@ const Home: NextPage = () => {
           </div>
 
           {authenticated &&
-            <div className='w-full py-6 text-center'>
-              <Link href="/subs/create">
-                <a className='w-full p-2 text-center text-white bg-gray-400 rounded'>
-                  커뮤니티 만들기
+            <div className='w-full py-4 text-center'>
+              <Link href="/subs/list">
+                <a className='w-full px-3 py-1 text-center text-white bg-fuchsia-800 rounded text-sm'>
+                  전체 커뮤니티
                 </a>
               </Link>
             </div>
           }
         </div>
+
+        <div className='bg-white border rounded'>
+          <div className='p-2 border-b rounded-t'
+            style={{
+              backgroundImage: `linear-gradient(rgba(135, 80, 156, 0.9), rgba(135, 80, 156, 0.9)), url(/tree-2249363_1920.jpg)`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          >
+            <p className='text-lg font-semibold text-center text-white'>MENU</p>
+          </div>
+
+
+          {authenticated &&
+          <div>
+            <div className='w-full pt-4 pb-2 text-center'>
+              <Link href="/subs/create">
+                <a className='w-full px-16 py-1 text-center text-white bg-fuchsia-800 rounded-full text-sm'>
+                  커뮤니티 생성
+                </a>
+              </Link>
+            </div>
+              <div className='w-full pt-2 pb-4 text-center'>
+              <Link href="/posts/create">
+                  <a className='w-full px-16 py-1 text-center text-fuchsia-800 border border-fuchsia-800 rounded-full text-sm'>
+                  프스트 생성
+                </a>
+              </Link>
+            </div>
+            </div>
+          }
+        </div>
       </div>
+
+      
 
     </div>
   )
