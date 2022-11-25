bd62608c1985979a26f0af58b26a7b0c30ccb25a
ex1 <skowko6750@naver.com>
2022-11-25 17:40:17 +0900

    네브바 디자인 수정

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -45,7 +45,7 @@ const NavBar: React.FC = () => {
     }, []);
 
     return (
-        <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-between h-14 px-4 bg-white'>
+        <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-between h-12 px-4 bg-white'>
             <div className='text-2xl font-semibold text-gray-400'>
                 <Link href="/">
                     {/* Community */}
@@ -62,12 +62,12 @@ const NavBar: React.FC = () => {
             </div>
 
             <div className='max-w-full px-4'>
-                <div className='relative flex items-center h-7 bg-gray-100 border rounded hover:border-gray-700 hover:bg-white'>
+                <div className='relative flex items-center h-7 bg-gray-100 border rounded hover:border-fuchsia-800 hover:bg-white'>
                     <FaSearch className='ml-2 text-gray-400' />
                     <input
                         type="text"
-                        placeholder='Search Reddit'
-                        className='px-3 py-1 bg-transparent rounded focus:outline-none'
+                        placeholder='Search...'
+                        className='px-3 py-1 bg-transparent rounded focus:outline-none text-sm'
                     />
                 </div>
             </div>
@@ -76,7 +76,7 @@ const NavBar: React.FC = () => {
             {/* md 사이즈 이하 일때 계정 아이콘 표시 */}
             {!loading &&
             <>
-                <div className='md:hidden relative border hover:cursor-pointer text-gray-500 hover:text-fuchsia-700 min-w-fit' onClick={handleMenu} ref={menuDropBox}>
+                <div className='md:hidden relative border hover:cursor-pointer text-gray-500 hover:text-fuchsia-800 min-w-fit' onClick={handleMenu} ref={menuDropBox}>
                     <i
                         className='fas fa-user-circle fa-lg my-1 mx-2 text-inherit' />
                     <i
@@ -84,24 +84,24 @@ const NavBar: React.FC = () => {
                     {isOpen && (authenticated ? 
                         (<div className='absolute border w-36 right-0 top-10 bg-white rounded'>
                                 <a 
-                                className='w-full text-center text-sm block py-2 hover:bg-fuchsia-700 hover:text-white font-semibold text-gray-600'
+                                className='w-full text-center text-sm block py-2 hover:bg-fuchsia-800 hover:text-white font-semibold text-gray-600'
                                     onClick={handleLogout}>
                                     로그아웃
                                 </a>
                             <Link href={`/u/${user?.username}`}>
-                                <a className='w-full text-center text-sm block py-2 hover:bg-fuchsia-700 hover:text-white font-semibold text-gray-600'>
+                                <a className='w-full text-center text-sm block py-2 hover:bg-fuchsia-800 hover:text-white font-semibold text-gray-600'>
                                     마이페이지
                                 </a>
                             </Link>
                         </div>) 
                         : (<div className='absolute border w-36 right-0 top-10 bg-white rounded'>
                             <Link href="/login">
-                                <a className='w-full text-center text-sm block py-2 hover:bg-fuchsia-700 hover:text-white font-semibold text-gray-600'>
+                                <a className='w-full text-center text-sm block py-2 hover:bg-fuchsia-800 hover:text-white font-semibold text-gray-600'>
                                     로그인
                                 </a>
                             </Link>
                             <Link href="/register">
-                                <a className='w-full text-center text-sm block py-2 hover:bg-fuchsia-700 hover:text-white font-semibold text-gray-600'>
+                                <a className='w-full text-center text-sm block py-2 hover:bg-fuchsia-800 hover:text-white font-semibold text-gray-600'>
                                     회원가입
                                 </a>
                             </Link>
@@ -116,7 +116,7 @@ const NavBar: React.FC = () => {
                     {!loading && (
                         authenticated ? (
                             <button
-                                className='w-20 px-2 mr-2 text-center text-sm text-white bg-fuchsia-700 rounded h-7'
+                                className='w-20 px-2 mr-2 text-center text-sm text-white bg-fuchsia-800 rounded h-7'
                                 onClick={handleLogout}
                             >
                                 로그아웃
@@ -124,12 +124,12 @@ const NavBar: React.FC = () => {
                         ) : (
                             <>
                                 <Link href="/login">
-                                    <a className='w-20 px-2 pt-1 mr-2 text-center text-sm text-fuchsia-700 border border-fuchsia-700 rounded h-7'>
+                                    <a className='w-20 px-2 pt-1 mr-2 text-center text-sm text-fuchsia-800 border border-fuchsia-800 rounded h-7'>
                                         로그인
                                     </a>
                                 </Link>
                                 <Link href="/register">
-                                        <a className='w-20 px-2 pt-1 text-center text-sm text-white bg-fuchsia-700 rounded h-7'>
+                                        <a className='w-20 px-2 pt-1 text-center text-sm text-white bg-fuchsia-800 rounded h-7'>
                                         회원가입
                                     </a>
                                 </Link>
