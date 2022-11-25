fab2bcbbaf5974c01c4760aaed33b47a7e4e7c6e
ex1 <skowko6750@naver.com>
2022-11-25 17:41:03 +0900

    포스트 생성페이지 생성(메인페이지에서 이동하는)

---
+++ client/src/pages/posts/create.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotImplemented from '../test/not_implemented'
+
+const CreatePost = () => {
+  return (
+    <div>
+        <NotImplemented></NotImplemented>
+    </div>
+  )
+}
+
+export default CreatePost
