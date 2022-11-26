1d4330075336d0225d637866ec7a1f7b74f54b2f
ex1 <skowko6750@naver.com>
2022-11-26 21:40:42 +0900

    캐로셀 구현

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -11,7 +11,12 @@ import useSWRInfinite from 'swr/infinite'
 import PostCard from '../components/PostCard'
 import { useEffect, useState } from 'react'
 
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
 const Home: NextPage = () => {
+  const banners = ['/cat.jpg', '/tree-2249363_1920.jpg', '/cat02.jpg', '/bug01.jpg']  // 배너 테스트용 -> 나중에 데이터로 받아옴
+
   const { authenticated } = useAuthState();
   const fetcher = async (url: string) => {
     return await axios.get(url).then(res => res.data)
@@ -29,6 +34,19 @@ const Home: NextPage = () => {
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
 
   const [observedPost, setObservedPost] = useState("");
+  const [slideNumber, setSlideNumber] = useState(0)
+
+
+  type slideType = {
+    [key: number]: string;
+  };
+
+  const gridColumns: slideType = {
+    0: "flex justify-content w-[400%] -ml-[0%] transition-all ",
+    1: "flex justify-content w-[400%] -ml-[100%] transition-all ",
+    2: "flex justify-content w-[400%] -ml-[200%] transition-all ",
+    3: "flex justify-content w-[400%] -ml-[300%] transition-all ",
+  };
 
   useEffect(() => {
     // 포스트가 없다면 return
@@ -40,6 +58,19 @@ const Home: NextPage = () => {
       observeElement(document.getElementById(id));
     }
   }, [posts])
+
+  const handleArrowBack = () => {
+    if (slideNumber !== 0) {
+      setSlideNumber(slideNumber - 1)
+    } else setSlideNumber(banners.length - 1)
+  }
+
+  const handleArrowForward = () => {
+    if (slideNumber !== (banners.length - 1)) {
+      setSlideNumber(slideNumber + 1)
+    } else setSlideNumber(0)
+
+  }
 
   const observeElement = (element: HTMLElement | null) => {
     if (!element) return;
@@ -59,7 +90,6 @@ const Home: NextPage = () => {
 
   }
 
-  const ar = ['/cat.jpg', '/tree-2249363_1920.jpg', '/cat02.jpg', '/bug01.jpg']
 
 
   return (
@@ -67,15 +97,15 @@ const Home: NextPage = () => {
       <div className='hidden md:block'>
         <div className='justify-content flex mb-4'>
           {
-            ar?.map(p => (
+            banners?.map(banner => (
               <div className='h-36  bg-gray-500 flex-auto first:ml-0 mx-1 last:mr-0 rounded relative'
                 style={{
-                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.9)), url(${p})`,
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.9)), url(${banner})`,
                   backgroundRepeat: 'no-repeat',
                   backgroundSize: 'cover',
                   backgroundPosition: 'center',
                 }}
-                key={p}><div className='absolute text-white bottom-0 left-0 right-0 m-2'>
+                key={banner}><div className='absolute text-white bottom-0 left-0 right-0 m-2'>
                   <div className='truncate'>배너 테스트 타이틀</div>
                   <div className='truncate w-inherit text-xs'>배너 테스트 내용입니닷!어쩌구 저쩌구 이!어쩌구 저쩌구 이!어쩌구 저쩌구 이!어쩌구 저쩌구 이러쩌구 저쩌구 이!어쩌구 저쩌구 이러쿵</div>
                 </div></div>
@@ -85,18 +115,31 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className='md:hidden  mb-4'>
-        <div className='h-36  bg-gray-500 first:ml-0 mx-1 last:mr-0 rounded relative'
-          style={{
-            backgroundImage: `url(${ar[0]})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}>
-          <div className='absolute text-white bottom-0 left-0 right-0 m-2'>
-            <div className='truncate'>배너 테스트 타이틀</div>
-            <div className='truncate w-inherit text-xs'>배너 테스트 내용입니닷!어쩌구 저쩌구 이!어쩌구 저쩌구 이!어쩌구 저쩌구 이!어쩌구 저쩌구 이러쩌구 저쩌구 이!어쩌구 저쩌구 이러쿵</div>
-          </div>
+
+      {/* md 사이즈 이하일때 배너(캐러셀형식으로 만들기)  */}
+      <div className='overflow-hidden w-inherit relative border mb-3 md:hidden hover:cursor-pointer'>
+        <div className={gridColumns[slideNumber]}>
+          <div className='absolute z-10 top-[50%] translate-y-[-50%] left-2 bg-gray-500 opacity-50 rounded-full text-white'
+            onClick={handleArrowBack}><IoIosArrowBack className='text-4xl' /></div>
+          <div className='absolute z-10 top-[50%] translate-y-[-50%] right-2 bg-gray-500 opacity-50 rounded-full text-white'
+            onClick={handleArrowForward}><IoIosArrowForward className='text-4xl' /></div>
+          {
+            banners?.map(banner => (
+              <div className='h-36 bg-gray-500 rounded relative flex-none w-[25%]'
+                style={{
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.9)), url(${banner})`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+                key={banner}><div className='absolute text-white bottom-0 left-0 right-0 m-2'>
+                  <div className='truncate'>배너 테스트 타이틀</div>
+                  <div className='truncate w-inherit text-xs'>배너 테스트 내용입니닷!어쩌구 저쩌구 이!어쩌구 저쩌구 이!어쩌구 저쩌구 이!어쩌구 저쩌구 이러쩌구 저쩌구 이!어쩌구 저쩌구 이러쿵</div>
+                </div>
+              </div>
+            )
+            )
+          }
         </div>
       </div>
 
