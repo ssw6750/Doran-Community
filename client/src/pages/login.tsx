8c03afa5f1f90786e8868964a22f4e58c3520bcf
ex1 <skowko6750@naver.com>
2022-11-26 21:41:09 +0900

    로그인/회원가입 페이지 로고 삽입

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -4,67 +4,83 @@ import Link from 'next/link'
 import { useRouter } from 'next/router';
 import InputGroup from '../components/InputGroup'
 import { useAuthDispatch, useAuthState } from '../context/auth';
+import Image from 'next/image';
 
 const Login = () => {
-    let router = useRouter()
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState<any>({});
-    const {authenticated} = useAuthState();
-    
-    const dispatch = useAuthDispatch();
+  let router = useRouter()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
+  const { authenticated } = useAuthState();
 
-    if (authenticated) router.push("/");
+  const dispatch = useAuthDispatch();
 
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault()
+  if (authenticated) router.push("/");
 
-        try{
-            const res = await axios.post("/auth/login", {password, username}, {withCredentials: true})
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
 
-            dispatch("LOGIN", res.data?.user);
+    try {
+      const res = await axios.post("/auth/login", { password, username }, { withCredentials: true })
 
-            router.push("/")
-            // router.push("/subs/create")
+      dispatch("LOGIN", res.data?.user);
 
-        } catch (error: any) {
-            console.log(error)
-            setErrors(error.response.data || {})
-        }
+      router.push("/")
+      // router.push("/subs/create")
+
+    } catch (error: any) {
+      console.log(error)
+      setErrors(error.response.data || {})
     }
-    return (
-       <div className='bg-white'>
-          <div className='flex flex-col itmes-center justify-center h-screen p-6'>
-              <div className='w-10/12 mx-auto md:w-96'>
-                  <h1 className='mb-2 text-lg font-medium'>로그인</h1>
-                  <form onSubmit={handleSubmit}>
-                    <InputGroup
-                      placeholder='Username'
-                      value={username}
-                      setValue={setUsername}
-                      error={errors.username}
-                    />
-                    <InputGroup
-                      placeholder='Password'
-                      value={password}
-                      setValue={setPassword}
-                      error={errors.password}
-                    />
-                    <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
-                      로그인
-                    </button>
-                  </form>
-                  <small>
-                    아직 아이디가 없나요?
-                    <Link href="/register">
-                      <a className='ml-1 text-blue-500 uppercase'>회원가입</a>
-                    </Link>
-                  </small>
+  }
+  return (
+    <div className='bg-white'>
+      <div className='flex flex-col itmes-center justify-center h-screen p-6'>
+        <div className='w-10/12 mx-auto md:w-96'>
+          <div className='mb-6'>
+            <Link href="/">
+              {/* Community */}
+              <a className='flex'>
+                <Image
+                  src="/11.png"
+                  alt="logo"
+                  width={500}
+                  height={130}
+                >
+                </Image>
+              </a>
+            </Link>
+          </div>
+          <h1 className='mb-2 text-lg font-medium'>로그인</h1>
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder='Username'
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder='Password'
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+              type='password'
+            />
+            <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
+              로그인
+            </button>
+          </form>
+          <small>
+            아직 아이디가 없나요?
+            <Link href="/register">
+              <a className='ml-1 text-blue-500 uppercase'>회원가입</a>
+            </Link>
+          </small>
 
-              </div>
-          </div>      
+        </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Login
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Image from 'next/image';
 import Link from 'next/link'
 import { useRouter } from 'next/router';
 import React, { FormEvent, useState } from 'react'
@@ -10,7 +11,7 @@ const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
-  const {authenticated} = useAuthState();
+  const { authenticated } = useAuthState();
 
   let router = useRouter();
   if (authenticated) router.push("/");
@@ -18,7 +19,7 @@ const Register = () => {
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
 
-    try{
+    try {
       const res = await axios.post('/auth/register', {
         email,
         password,
@@ -29,46 +30,60 @@ const Register = () => {
     } catch (error: any) {
       console.log('error', error);
       setErrors(error.response.data || {})
-    } 
-  } 
+    }
+  }
 
   return (
     <div className='bg-white'>
-        <div className='flex flex-col itmes-center justify-center h-screen p-6'>
-            <div className='w-10/12 mx-auto md:w-96'>
-                <h1 className='mb-2 text-lg font-medium'>회원가입</h1>
-                <form onSubmit={handleSubmit}>
-                  <InputGroup
-                    placeholder='Email'
-                    value={email}
-                    setValue={setEmail}
-                    error={errors.email}
-                  />
-                  <InputGroup
-                    placeholder='Username'
-                    value={username}
-                    setValue={setUsername}
-                    error={errors.username}
-                  />
-                  <InputGroup
-                    placeholder='Password'
-                    value={password}
-                    setValue={setPassword}
-                    error={errors.password}
-                  />
-                  <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
-                    회원 가입
-                  </button>
-                </form>
-                <small>
-                  이미 가입하셨나요?
-                  <Link href="/login">
-                    <a className='ml-1 text-blue-500 uppercase'>로그인</a>
-                  </Link>
-                </small>
-                
-            </div>
-        </div>      
+      <div className='flex flex-col itmes-center justify-center h-screen p-6'>
+        <div className='w-10/12 mx-auto md:w-96'>
+          <div className='mb-6'>
+            <Link href="/">
+              {/* Community */}
+              <a className='flex'>
+                <Image
+                  src="/11.png"
+                  alt="logo"
+                  width={500}
+                  height={130}
+                >
+                </Image>
+              </a>
+            </Link>
+          </div>
+          <h1 className='mb-2 text-lg font-medium'>회원가입</h1>
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder='Email'
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder='Username'
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder='Password'
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
+            <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
+              회원 가입
+            </button>
+          </form>
+          <small>
+            이미 가입하셨나요?
+            <Link href="/login">
+              <a className='ml-1 text-blue-500 uppercase'>로그인</a>
+            </Link>
+          </small>
+
+        </div>
+      </div>
     </div>
   )
 }
