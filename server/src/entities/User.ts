a2cbd761d2a1b508f60ab66099397f72d79a0b2a
= <=>
2023-06-26 20:31:14 +0900

    유저테이블 role 컬럼 추가

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -6,32 +6,34 @@ import Vote from "./Vote";
 import bcrypt from 'bcryptjs'
 
 @Entity("users")
-export default class User extends BaseEntity{
+export default class User extends BaseEntity {
+  @Index()
+  @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
+  @Length(1, 255, { message: "이메일 주소는 비워둘 수 없습니다." })
+  @Column({ unique: true })
+  email: string;
 
-    @Index()
-    @IsEmail(undefined, {message: "이메일 주소가 잘못되었습니다."})
-    @Length(1, 255, {message: "이메일 주소는 비워둘 수 없습니다."})
-    @Column({unique: true})
-    email: string;
+  @Index()
+  @Length(3, 32, { message: "사용자 이름은 3자 이상이어야 합니다." })
+  @Column({ unique: true })
+  username: string;
 
-    @Index()
-    @Length(3, 32, {message: "사용자 이름은 3자 이상이어야 합니다."})
-    @Column({unique: true})
-    username: string
+  @Column()
+  @Length(6, 255, { message: "비밀번호는 6자리 이상이어야 합니다." })
+  password: string;
 
-    @Column()
-    @Length(6, 255, {message: '비밀번호는 6자리 이상이어야 합니다.'})
-    password: string
+  // enum 사용하면 좋을듯   
+  @Column({ nullable: true })
+  role: string;
 
-    @OneToMany(() => Post, (post) => post.user)
-    posts: Post[]
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
-    @OneToMany(() => Vote, (vote) => vote.user)
-    votes: Vote[]
+  @OneToMany(() => Vote, (vote) => vote.user)
+  votes: Vote[];
 
-    @BeforeInsert()
-    async hashPassword() {
-        this.password = await bcrypt.hash(this.password, 6)
-    }
-
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
 }
