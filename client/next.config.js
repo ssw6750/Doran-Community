b1daf275834a69732d70f251a6b55a60b2d956f4
= <=>
2023-06-29 22:07:44 +0900

    docker next js hot reloading을 위한 설정 추가

--- client/next.config.js
+++ client/next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
       "ec2-54-95-109-247.ap-northeast-1.compute.amazonaws.com",
     ],
   },
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
