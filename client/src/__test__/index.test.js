6d00ab7775f0053b56072d840a7dcc43a2aab050
= <=>
2023-06-29 23:47:08 +0900

    테스트 코드 설정

---
+++ client/src/__test__/index.test.js
@@ -0,0 +1,15 @@
+// __tests__/index.test.js
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    const { container } = render(<Home />);
+
+    const heading = screen.getByText("인기 게시판");
+    
+    expect(heading).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+});
