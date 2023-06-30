46d12e5b2d19fee3bc3eac5e8e59c3aa0a0baafa
= <=>
2023-06-30 23:51:08 +0900

    호스트 rds 연결 테스트

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -4,7 +4,8 @@ import { DataSource } from "typeorm"
 export const AppDataSource = new DataSource({
   type: "postgres",
   // host: "localhost",
-  host: "db",
+  // host: "db",
+  host: "docker-valonity.cgnoahowkfxn.ap-northeast-1.rds.amazonaws.com",
   port: 5432,
   username: "postgres",
   password: "password",
